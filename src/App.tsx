b2236162def7fa49cbba1b2aa929b00320705dd3
Nir Gluzman <nir.gluzman@gmail.com>
2025-05-31 21:20:23 +0200

    feat: display mock quiz data in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from 'react';
+
 import './App.css';
+import type { QUIZ } from './interfaces/QUIZ';
+import DATA from './assets/data.json';
 
 function App() {
-  return (
-    <>
-      <p>Hello World</p>
-    </>
-  );
+  const [data, setData] = useState<QUIZ[]>([]);
+
+  useEffect(() => {
+    // simulate fetching data from a JSON file
+    setData(DATA as QUIZ[]);
+  }, []);
+
+  return <pre>{JSON.stringify(data, null, 4)}</pre>;
 }
 
 export default App;
