600a74ab1f1f98ed45d1c928f1f106130334ff16
Nir Gluzman <nir.gluzman@gmail.com>
2025-06-01 10:50:02 +0200

    feat: create the question layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
 import { useEffect, useState } from 'react';
 
-import './App.css';
-import type { QUIZ } from './interfaces/QUIZ';
+import './App.scss';
+
 import DATA from './assets/data.json';
+
+import type { QUIZ } from './interfaces/QUIZ.ts';
+import { Quiz } from './components/quiz.tsx';
 
 function App() {
   const [data, setData] = useState<QUIZ[]>([]);
+  const [questionNo, setQuestionNo] = useState<number>(0);
 
   useEffect(() => {
     // simulate fetching data from a JSON file
     setData(DATA as QUIZ[]);
   }, []);
 
-  return <pre>{JSON.stringify(data, null, 4)}</pre>;
+  return (
+    <div className='container'>
+      <div className='subject'>Programming</div>
+      <div className='question-header'>
+        question: {questionNo + 1}/{data.length}
+      </div>
+      <div className='quiz'>
+        <Quiz {...data[questionNo]} />
+      </div>
+      <div className='nav nav-next'>
+        <button disabled={questionNo === data.length - 1} onClick={() => setQuestionNo((c) => c + 1)}>
+          Next
+        </button>
+      </div>
+      <div className='nav nav-prev'>
+        <button disabled={questionNo === 0} onClick={() => setQuestionNo((c) => c - 1)}>
+          Prev
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
